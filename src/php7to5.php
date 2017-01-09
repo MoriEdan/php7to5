4528ea38b7bc1ec838942f32c8d94e5ce289bc9a
Kevin Davies <kdavies@gmail.com>
2017-01-09 14:52:38 +1300

    Second commit.

--- src/php7to5.php
+++ src/php7to5.php
@@ -5,8 +5,12 @@ declare(strict_types = 1); // must be first line
 namespace premiumwebtechnologies\php7to5;
 
 
-class PHP7To5
+class PHP7To5 implements IPHP7To5
 {
 
-
+    public static function parse_content(string $content):string
+    {
+        // Remove ):[type]; from end of lines.
+        
+    }
 }
