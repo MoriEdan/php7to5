f365a16c06ffbd15ec744ad31b04baba84dd1178
Kevin Davies <kdavies@gmail.com>
2017-01-09 15:24:01 +1300

    Added code to remove return type declarations.

--- src/php7to5.php
+++ src/php7to5.php
@@ -11,7 +11,15 @@ class PHP7To5 implements IPHP7To5
     public static function parse_content(string $content):string
     {
         // Remove ):[type]; from end of lines.
-        $content = preg_replace("/\)\s*[string|int|float|bool]\s+\;/");
+        $content = preg_replace("/\)\s*\:[a-zA-Z]*\s*\;/", ");", $content);
+
+        // Remove ):[type]{ from end of lines.
+        $content = preg_replace("/\)\s*\:[a-zA-Z]*\s*\{/", "){", $content);
+
+        // Remove ):[type] from end of lines.
+        $content = preg_replace("/\)\s*\:[a-zA-Z]*\s*$/", ")", $content);
+
+        return $content;
     }
 
 }
--- tests/PHP7To5Test.php
+++ tests/PHP7To5Test.php
@@ -1,46 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kevindavies
- * Date: 9/01/17
- * Time: 2:55 PM
- */
 
-// Configure this file by going to Preferences -> Editor -> File and Code Templates
-// To set coding standard go to Preferences -> Editor -> Inspections -> PHP -> PHP Code Sniffer validation
-// [in coding standards drop down select desired coding standard]
-// To set PHP version to use click on External Libraries on left pane and select Configure PHP include paths.
-// To verify this file go to Code -> Inspect code
-// To view error log go to /Applications/MAMP/logs
-// To install a composer package right click on project name, click on composer and select init. Once done select
-// composer again and select add dependency.
-// To add a bookmark click fn+F3
-// To show bookmarks click fn+cmd+F3
-// To go to a bookmark click ctl+[0=9]
-// To format cmd+alt+L
-// To duplicate a line(s): cmd+d
+include('src/iphp7to5.php');
+include('src/php7to5.php');
+
 
 class PHP7To5Test extends PHPUnit_Framework_TestCase
 {
 
-    public function setUp()
+    public function testParseContent()
     {
 
-    }
+        $content = "\nfunction abc():string;\nfunction abc():int;\nfunction abc():float;\nfunction abc():bool;";
+        $content = \premiumwebtechnologies\php7to5\PHP7To5::parse_content($content);
+        $this->assertTrue($content=="\nfunction abc();\nfunction abc();\nfunction abc();\nfunction abc();", "true didn't end up being false!");
 
-    public function tearDown()
-    {
+        $content = "\nfunction abc():string{";
+        $content = \premiumwebtechnologies\php7to5\PHP7To5::parse_content($content);
+        $this->assertTrue($content=="\nfunction abc(){", "true didn't end up being false!");
 
-    }
+        $content = "\nfunction abc():string";
+        $content = \premiumwebtechnologies\php7to5\PHP7To5::parse_content($content);
+        $this->assertTrue($content=="\nfunction abc()", "true didn't end up being false!");
 
-    public function testMinimumViableTest()
-    {
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->assertTrue(false, "true didn't end up being false!");
-    }
-
-    public function testMethodName()
-    {
 
     }
 
